f1fbf5b8d2f64a3220a16fab7d873ce93e18d285
EAntunes12 <ernestoantunes5@gmail.com>
2026-05-11 17:59:22 -0300

    first commit

---
+++ src/main/java/da/ort/obligatorioErnestoAntunes/ObligatorioErnestoAntunesApplication.java
@@ -0,0 +1,13 @@
+package da.ort.obligatorioErnestoAntunes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObligatorioErnestoAntunesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObligatorioErnestoAntunesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/da/ort/obligatorioErnestoAntunes/ObligatorioErnestoAntunesApplicationTests.java
@@ -0,0 +1,13 @@
+package da.ort.obligatorioErnestoAntunes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObligatorioErnestoAntunesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
